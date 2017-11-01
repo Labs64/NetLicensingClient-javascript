8e05399e25f6566855887fd1f3324ebefa66d252
Slava Rudkovskiy <xraskolx@gmail.com>
2017-11-01 14:37:24 +0300

    Prepare for release 0.5.7

--- dist/netlicensing-client.js
+++ dist/netlicensing-client.js
@@ -3671,56 +3671,10 @@ NetLicensing.Service = (function () {
 
         var __httpXHR = {};
 
-        var Base64 = {
-            _keyStr: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
-            encode: function (e) {
-                var t = "";
-                var n, r, i, s, o, u, a;
-                var f = 0;
-                e = Base64._utf8_encode(e);
-                while (f < e.length) {
-                    n = e.charCodeAt(f++);
-                    r = e.charCodeAt(f++);
-                    i = e.charCodeAt(f++);
-                    s = n >> 2;
-                    o = (n & 3) << 4 | r >> 4;
-                    u = (r & 15) << 2 | i >> 6;
-                    a = i & 63;
-                    if (isNaN(r)) {
-                        u = a = 64
-                    } else if (isNaN(i)) {
-                        a = 64
-                    }
-                    t = t + this._keyStr.charAt(s) + this._keyStr.charAt(o) + this._keyStr.charAt(u) + this._keyStr.charAt(a)
-                }
-                return t
-            },
-            decode: function (e) {
-                var t = "";
-                var n, r, i;
-                var s, o, u, a;
-                var f = 0;
-                e = e.replace(/[^A-Za-z0-9+/=]/g, "");
-                while (f < e.length) {
-                    s = this._keyStr.indexOf(e.charAt(f++));
-                    o = this._keyStr.indexOf(e.charAt(f++));
-                    u = this._keyStr.indexOf(e.charAt(f++));
-                    a = this._keyStr.indexOf(e.charAt(f++));
-                    n = s << 2 | o >> 4;
-                    r = (o & 15) << 4 | u >> 2;
-                    i = (u & 3) << 6 | a;
-                    t = t + String.fromCharCode(n);
-                    if (u != 64) {
-                        t = t + String.fromCharCode(r)
-                    }
-                    if (a != 64) {
-                        t = t + String.fromCharCode(i)
-                    }
-                }
-                t = Base64._utf8_decode(t);
-                return t
-            },
-            _utf8_encode: function (e) {
+        var base64Encode = function (e) {
+            var _keyStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+
+            var utf8_encode = function (e) {
                 e = e.replace(/rn/g, "n");
                 var t = "";
                 for (var n = 0; n < e.length; n++) {
@@ -3737,29 +3691,29 @@ NetLicensing.Service = (function () {
                     }
                 }
                 return t
-            },
-            _utf8_decode: function (e) {
-                var t = "";
-                var n = 0;
-                var r = c1 = c2 = 0;
-                while (n < e.length) {
-                    r = e.charCodeAt(n);
-                    if (r < 128) {
-                        t += String.fromCharCode(r);
-                        n++
-                    } else if (r > 191 && r < 224) {
-                        c2 = e.charCodeAt(n + 1);
-                        t += String.fromCharCode((r & 31) << 6 | c2 & 63);
-                        n += 2
-                    } else {
-                        c2 = e.charCodeAt(n + 1);
-                        c3 = e.charCodeAt(n + 2);
-                        t += String.fromCharCode((r & 15) << 12 | (c2 & 63) << 6 | c3 & 63);
-                        n += 3
-                    }
+            };
+
+            var t = "";
+            var n, r, i, s, o, u, a;
+            var f = 0;
+            e = utf8_encode(e);
+            while (f < e.length) {
+                n = e.charCodeAt(f++);
+                r = e.charCodeAt(f++);
+                i = e.charCodeAt(f++);
+                s = n >> 2;
+                o = (n & 3) << 4 | r >> 4;
+                u = (r & 15) << 2 | i >> 6;
+                a = i & 63;
+                if (isNaN(r)) {
+                    u = a = 64
+                } else if (isNaN(i)) {
+                    a = 64
                 }
-                return t
+                t = t + _keyStr.charAt(s) + _keyStr.charAt(o) + _keyStr.charAt(u) + _keyStr.charAt(a)
             }
+            return t
+
         };
 
         instance.getLastHttpRequestInfo = function () {
@@ -3906,12 +3860,12 @@ NetLicensing.Service = (function () {
                     if (!context.getUsername()) throw new Error('Missing parameter "username"');
                     if (!context.getPassword()) throw new Error('Missing parameter "password"');
 
-                    headers['Authorization'] = 'Basic ' + Base64.encode(context.getUsername() + ':' + context.getPassword());
+                    headers['Authorization'] = 'Basic ' + base64Encode(context.getUsername() + ':' + context.getPassword());
                     break;
                 case NetLicensing.Context.APIKEY_IDENTIFICATION:
                     if (!context.getApiKey()) throw new Error('Missing parameter "apiKey"');
 
-                    headers['Authorization'] = 'Basic ' + Base64.encode('apiKey:' + context.getApiKey());
+                    headers['Authorization'] = 'Basic ' + base64Encode('apiKey:' + context.getApiKey());
                     break;
                 default:
                     throw new Error('Unknown security mode');
