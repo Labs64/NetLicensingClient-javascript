31b8adaf9027f74a5d0e5533116130da7b095b16
R.Brown <ready.brown@hotmail.de>
2020-10-14 20:13:30 +0200

    Update LicenseeService.js

--- src/services/LicenseeService.js
+++ src/services/LicenseeService.js
@@ -158,7 +158,10 @@ export default {
     },
 
     /**
-     * Validates active licenses of the licensee. See NetLicensingAPI for details:
+     * Validates active licenses of the licensee.
+     * In the case of multiple product modules validation,
+     * required parameters indexes will be added automatically.
+     * See NetLicensingAPI for details:
      * @see https://netlicensing.io/wiki/licensee-services#validate-licensee
      *
      * @param context NetLicensing.Context
