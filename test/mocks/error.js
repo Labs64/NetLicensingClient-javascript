28edc9713744b4d90479ce54daf573120fa9a161
Viacheslav Rudkovskiy <xraskolx@gmail.com>
2021-09-03 16:09:08 +0300

    - fix vulnerabilities in dependencies - update dependencies - cleanup

--- test/mocks/error.js
+++
@@ -1,8 +0,0 @@
-export default (...args) => {
-    const template = { infos: { info: [] } };
-    args.forEach((arg) => {
-        const [id, value] = arg;
-        template.infos.info.push({ id, value, type: 'Error' });
-    });
-    return template;
-};
