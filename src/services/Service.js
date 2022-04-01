63efcc022835dc25fab435942282c26bc951ea4b
Viacheslav Rudkovskiy <xraskolx@gmail.com>
2022-04-01 17:14:42 +0300

    remove unused methods

--- src/services/Service.js
+++ src/services/Service.js
@@ -216,17 +216,6 @@ export default class Service {
         }
     }
 
-    static isValidUrl(url) {
-        const pattern = new RegExp('^(https?:\\/\\/)?' // protocol
-            + '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|' // domain name
-            + '((\\d{1,3}\\.){3}\\d{1,3}))' // OR ip (v4) address
-            + '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' // port and path
-            + '(\\?[;&a-z\\d%_.~+=-]*)?' // query string
-            + '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
-
-        return pattern.test(url);
-    }
-
     static toQueryString(data, prefix) {
         const query = [];
 
