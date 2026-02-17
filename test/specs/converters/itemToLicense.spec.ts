f1680ffc2e1100e5e9f038977a7008a4cabd3755
Viacheslav Rudkovskyi <xraskolx@gmail.com>
2026-02-17 15:39:16 +0200

    cleanup update dependencies

--- test/specs/converters/itemToLicense.spec.ts
+++ test/specs/converters/itemToLicense.spec.ts
@@ -10,7 +10,6 @@ import License from '@/entities/License';
 import { TimeVolumePeriodValues } from "@/types";
 import { Item } from '@/types/api/response';
 import { LicenseProps } from '@/types/entities/License';
-import { TimeVolumePeriodValues } from "@/types";
 
 describe('itemToLicense converter', () => {
   it('should correctly convert item to License', () => {
